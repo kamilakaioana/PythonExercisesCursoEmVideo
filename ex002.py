c300c881ff2a17bef5a390e34430b6aac99908d8
Kamila Kaioana <50750571+kamilakaioana@users.noreply.github.com>
2019-05-20 12:44:45 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# Crie um script python, que leia o dia o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+dia= input('dia=')
+mes=input('mes=')
+ano=input('ano=')
+print('voce nasceu no dia', dia,'de', mes, 'de', ano,'correto?')
