3d5a216003d1396142efdd44ae28b4f70d17a672
Kamila Kaioana <50750571+kamilakaioana@users.noreply.github.com>
2019-05-20 12:52:28 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,6 @@
+#Crie um Script Python que leia dois numeros e tente mostrar a soma entre eles.
+
+primeiro=input('informe o primeiro numero:')
+segundo=input('informe o segundo numero:')
+soma=int(primeiro)+int(segundo)
+print('a soma é', soma)
