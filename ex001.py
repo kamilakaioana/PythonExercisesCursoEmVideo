fc368db897adf4eb2eb156335396022c64207131
Kamila Kaioana <50750571+kamilakaioana@users.noreply.github.com>
2019-05-20 12:45:55 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,4 +1,4 @@
 #Crie um Script python que leia um nome de uma pessoa e mostre uma mensagem de boa vindas, de acordo com o valor digitado.
 
 nome=input('qual é seu nome?')
-print('Olá', nome, '!', 'Prazer em te cohecer!')
+print('Olá', nome, '!', 'Prazer em te conhecer!')
